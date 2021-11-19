cf3228d61b9d3632343cb6a6ad6fbe50b68e672c
Renan Bueno <renanbuenov@hotmail.com>
2021-11-18 23:57:25 -0300

    Component update created

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,6 @@
+import { Product } from './../product.model';
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  product: Product = {
+    name: '',
+    price: 0
   }
 
+  constructor(private productService: ProductService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto Criado!')
+      this.router.navigate(['/products'])
+    })
+    
+  }
+
+  cancelarProduct(): void {
+    this.router.navigate(['/products'])
+  }
 }
--- src/app/directives/for.directive.ts
+++ src/app/directives/for.directive.ts
@@ -6,8 +6,7 @@ import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angula
 export class ForDirective implements OnInit {
 
   @Input('myForEm') numbers!: number[];
-  @Input('myForUsando') texto!: String;
-
+ 
   constructor(
     private containers: ViewContainerRef, 
     private template: TemplateRef<any>
